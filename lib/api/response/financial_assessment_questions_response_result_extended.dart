4da09b817fc6724b92578443389b0148612bf9cd
zohreh-deriv <zohreh@deriv.com>
2025-06-12 17:14:56 +0800

    add fetch method to fetch FinancialAssessment Questions

--- lib/api/response/financial_assessment_questions_response_result_extended.dart
+++ lib/api/response/financial_assessment_questions_response_result_extended.dart
@@ -2,7 +2,8 @@ import 'package:deriv_dependency_injector/dependency_injector.dart';
 import 'package:flutter_deriv_api/api/exceptions/base_api_exception.dart';
 import 'package:flutter_deriv_api/api/models/base_exception_model.dart';
 import 'package:flutter_deriv_api/api/response/financial_assessment_questions_response_result.dart';
-import 'package:flutter_deriv_api/basic_api/generated/api.dart';
+import 'package:flutter_deriv_api/basic_api/generated/financial_assessment_questions_receive.dart';
+import 'package:flutter_deriv_api/basic_api/generated/financial_assessment_questions_send.dart';
 import 'package:flutter_deriv_api/helpers/helpers.dart';
 import 'package:flutter_deriv_api/services/connection/api_manager/base_api.dart';
 
